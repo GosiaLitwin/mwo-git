4b04eb2dc15bebe541ae4deb00de15d8e9dbb1ab
Gosia Litwin <malgosia.litwin@gmail.com>
2017-02-11 14:23:18 +0100

    further implmentation

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,5 +2,14 @@ public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
 		System.out.println("We will be Git experts, soon.");
+
     }
-}
+	public void managing-satellite ()  {
+	//todo implement
+	}
+public void manage-rocket ()  {
+	//todo implement
+	}
+	}
+	
+	
